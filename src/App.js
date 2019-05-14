4072100cb5712ff57fb0a52b0ecc7e55c911894f
Matt Henry <matthew.w.henry@gmail.com>
2019-05-14 19:41:09 -0400

    add toggle for location

--- src/App.js
+++ src/App.js
@@ -6,19 +6,28 @@ import { LineChart } from './components/LineChart'
 
 function App() {
   const [data, setData] = useState([]);
+  const [loc, setLoc] = useState("sf");
 
   useEffect(() => {
     const fetchData = async () => {
-      const resp = await fetch('https://raw.githubusercontent.com/sxywu/react-d3-example/master/public/sf.json');
+      const resp = await fetch(`https://raw.githubusercontent.com/sxywu/react-d3-example/master/public/${loc}.json`);
       const json = await resp.json();
-      setData((prevState) => [...prevState, ...json]);
+      setData(() => [...json]);
     }
 
     fetchData();
-  }, []);
+  }, [loc]);
+
+  const handleCityChange = (e) => {
+    setLoc(e.target.value)
+  }
 
   return (
     <div className="App">
+      <select defaultValue="sf" onChange={handleCityChange}>
+        <option value="sf">San Francisco</option>
+        <option value="ny">New York</option>
+      </select>
       <BarChart data={data} canvas={{ width: 600, height: 400 }} />
       <RadialChart data={data} canvas={{ width: 600, height: 600 }} />
       <LineChart data={data} canvas={{ width: 600, height: 400 }} />
